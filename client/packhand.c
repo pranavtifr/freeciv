d9e81638ff88ecf3951db4e46f80dd1b5de62019
Zeko <zeko@freecivweb.org>
2018-12-18 15:27:15 -0800

    Generate a message for every combat result

--- client/packhand.c
+++ client/packhand.c
@@ -476,6 +476,12 @@ void handle_unit_combat_info(int attacker_unit_id, int defender_unit_id,
   struct unit *punit0 = game_unit_by_number(attacker_unit_id);
   struct unit *punit1 = game_unit_by_number(defender_unit_id);
 
+  create_event(unit_tile(punit1), E_UNIT_LOST_ATT, ftc_client,
+               _("Combat result: defending %s (%d) %d hp, attacking %s"
+                 "(%d) %d hp."), unit_name_translation(punit1),
+               defender_unit_id, defender_hp, unit_name_translation(punit0),
+               attacker_unit_id, attacker_hp);
+
   if (punit0 && punit1) {
     popup_combat_info(attacker_unit_id, defender_unit_id, attacker_hp,
                       defender_hp, make_winner_veteran);
